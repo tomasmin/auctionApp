be6cbe28441f818ebbb4e1c62391506397b685ff
tommin <tomas.mineika@outlook.com>
2022-01-29 16:25:16 +0200

    nav and auction module init

---
+++ src/app/auctions/auction-item/auction-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auction-item',
+  templateUrl: './auction-item.component.html',
+  styleUrls: ['./auction-item.component.scss']
+})
+export class AuctionItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/auctions/auctions.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuctionItemComponent } from './auction-item/auction-item.component';
+
+
+
+@NgModule({
+  declarations: [
+    AuctionItemComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuctionsModule { }
